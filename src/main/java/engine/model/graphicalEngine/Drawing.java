b4f6e45e5b72e8189906b9f29d010249c90f78a8
Marius THORRE <thorre.m@yahoo.com>
2023-10-14 19:21:30 +0200

    Creation of shape and map instance in Drawing class

--- src/main/java/engine/model/graphicalEngine/Drawing.java
+++ src/main/java/engine/model/graphicalEngine/Drawing.java
@@ -1,4 +1,39 @@
 package engine.model.graphicalEngine;
 
-public class MovementManagement {
+import engine.model.graphicalEngine.map.FactoryMap;
+import engine.model.graphicalEngine.shape.FactoryShape;
+
+import java.util.List;
+
+public class Drawing {
+    private List<FactoryShape> factoryShapeList;
+    private FactoryShape shape;
+    private FactoryMap map;
+
+
+    public Drawing() {}
+
+    public List<FactoryShape> getFactoryShapeList() {*
+        return factoryShapeList;
+    }
+
+    public void setFactoryShapeList(List<FactoryShape> factoryShapeList) {
+        this.factoryShapeList = factoryShapeList;
+    }
+
+    public FactoryShape getShape() {
+        return shape;
+    }
+
+    public void setShape(FactoryShape shape) {
+        this.shape = shape;
+    }
+
+    public FactoryMap getMap() {
+        return map;
+    }
+
+    public void setMap(FactoryMap map) {
+        this.map = map;
+    }
 }
