1cf92aa6b8f37d2d5156b15fd2512218064c9850
Ennasser <nasserhan25@gmail.com>
2023-10-15 19:03:23 +0200

    implementing class movement

--- src/main/java/engine/model/physicalEngine/movement/movement.java
+++ src/main/java/engine/model/physicalEngine/movement/movement.java
@@ -1,5 +1,20 @@
 package engine.model.physicalEngine.movement;
 
-public class movement {
-    
+public abstract class movement {
+
+    protected Position position;
+    protected float velocityX;
+    protected float velocityY;
+
+
+    public void Movement(float x, float y, float baseVelocity) {
+        position = new Position(x, y);
+        velocityX = baseVelocity;
+        velocityY = baseVelocity;
+
+    }
+
+    public abstract void applyForce(float forceX, float forceY);
+
+    public abstract void update(float deltaTime);
 }
