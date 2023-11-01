da69246d64b95973e2079617feb44b39c3dcfeb9
Anis <anis.nemmiche@etu.univ-amu.fr>
2023-11-01 15:22:44 +0100

    graphicalengineAnis

---
+++ src/main/java/engine/model/graphicalEngineAnis/GraphicalEngine.java
@@ -0,0 +1,41 @@
+package engine.model.graphicalEngineAnis;
+
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class GraphicalEngine {
+    private GraphicsContext graphicsContext;
+    private Group playGround;
+    private Canvas canvas;
+
+    public GraphicalEngine(double width, double length) {
+        this.playGround = new Group();
+        this.canvas = new Canvas(width, length);
+        this.graphicsContext = canvas.getGraphicsContext2D();
+        this.playGround.getChildren().add(canvas);
+        drawMap(width, length, Color.BLACK);
+    }
+
+    public GraphicsContext getGraphicsContext() {
+        return this.graphicsContext;
+    }
+
+    public Group getPlayGround() {
+        return this.playGround;
+    }
+
+    public void drawMap(double width, double length, Color color) {
+        MapDrawing mapDrawing = new MapDrawing(width, length, color);
+        draw(mapDrawing);
+    }
+
+    public void draw(Drawing drawing) {
+        drawing.draw(this.graphicsContext);
+    }
+
+    public void clear() {
+        this.graphicsContext.clearRect(0, 0, this.canvas.getWidth(), this.canvas.getHeight());
+    }
+}
---
+++ src/main/java/engine/model/graphicalEngineAnis/RectangleDrawing.java
@@ -0,0 +1,46 @@
+package engine.model.graphicalEngineAnis;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class RectangleDrawing implements Drawing {
+    private double x;
+    private double y;
+    private double width;
+    private double length;
+    private Color color;
+
+    public RectangleDrawing(double x, double y, double width, double length, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.length = length;
+        this.color = color;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public void draw(GraphicsContext context) {
+        context.setFill(this.color);
+        context.fillRect(this.x, this.y, this.width, this.length);
+    }
+}
