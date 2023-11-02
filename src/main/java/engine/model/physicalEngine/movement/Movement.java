70ec3806c0801644995862f5996ef29b81a5a92c
AHAMADA Warren <warren.ahamada@etu.univ-amu.fr>
2023-11-02 12:08:10 +0100

    test Movement

--- src/main/java/engine/model/physicalEngine/movement/Movement.java
+++ src/main/java/engine/model/physicalEngine/movement/Movement.java
@@ -18,24 +18,24 @@ public class Movement {
      */
 
 
-    public void updatePositonX(Direction direction, Rectangle shape, Map map) {
-        if(!isColliding(shape, map, direction)){
+    public void updatePositonX(Direction direction, Rectangle rectangle, Map map) {
+        if(!isColliding(rectangle, map, direction)){
             if (direction == Direction.RIGHT) {
-                shape.setPosition(shape.getPosition().getX() + shape.getVelocity().getVelocityX(), shape.getPosition().getY());
+                rectangle.setPosition(rectangle.getPosition().getX() + rectangle.getVelocity().getVelocityX(), rectangle.getPosition().getY());
             }
             else if (direction == Direction.LEFT) {
-                shape.setPosition(shape.getPosition().getX() - shape.getVelocity().getVelocityX(), shape.getPosition().getY());
+                rectangle.setPosition(rectangle.getPosition().getX() - rectangle.getVelocity().getVelocityX(), rectangle.getPosition().getY());
             }
         }
     }
 
-    public void updatePositonY(Direction direction, Rectangle shape, Map map) {
-        if (!isColliding(shape, map, direction)) {
+    public void updatePositonY(Direction direction, Rectangle rectangle, Map map) {
+        if (!isColliding(rectangle, map, direction)) {
             if (direction == Direction.UP) {
-                shape.setPosition(shape.getPosition().getX(), shape.getPosition().getY() + shape.getVelocity().getVelocityY());
+                rectangle.setPosition(rectangle.getPosition().getX(), rectangle.getPosition().getY() + rectangle.getVelocity().getVelocityY());
             }
             else if (direction == Direction.DOWN) {
-                shape.setPosition(shape.getPosition().getX(), shape.getPosition().getY() - shape.getVelocity().getVelocityY());
+                rectangle.setPosition(rectangle.getPosition().getX(), rectangle.getPosition().getY() - rectangle.getVelocity().getVelocityY());
             }
         }
     }
@@ -48,27 +48,27 @@ public class Movement {
         this.direction = direction;
     }
 
-    public boolean isColliding(Rectangle shape, Map map, Direction direction) {
-        List<Position> apex = shape.getApex();
+    public boolean isColliding(Rectangle rectangle, Map map, Direction direction) {
+        List<Position> apex = rectangle.getApex();
         for (Rectangle shape2 : map.getShapeList()) {
-            if (shape2 != shape) {
+            if (shape2 != rectangle) {
                 List<Position> apex2 = shape2.getApex();
                 for (int i = 0; i < apex.size(); i++) {
                     switch (direction){
                         case UP -> {
-                            if (apex.get(i).getY() < apex2.get(i).getY() && apex.get(i).getY() + shape.getVelocity().getVelocityY() >= apex2.get(i).getY())
+                            if (apex.get(i).getY() < apex2.get(i).getY() && apex.get(i).getY() + rectangle.getVelocity().getVelocityY() >= apex2.get(i).getY())
                                 return true;
                         }
                         case DOWN -> {
-                            if (apex.get(i).getY() > apex2.get(i).getY() && apex.get(i).getY() - shape.getVelocity().getVelocityY() <= apex2.get(i).getY())
+                            if (apex.get(i).getY() > apex2.get(i).getY() && apex.get(i).getY() - rectangle.getVelocity().getVelocityY() <= apex2.get(i).getY())
                                 return true;
                         }
                         case RIGHT -> {
-                            if (apex.get(i).getX() < apex2.get(i).getX() && apex.get(i).getX() + shape.getVelocity().getVelocityX() >= apex2.get(i).getX())
+                            if (apex.get(i).getX() < apex2.get(i).getX() && apex.get(i).getX() + rectangle.getVelocity().getVelocityX() >= apex2.get(i).getX())
                                 return true;
                         }
                         case LEFT -> {
-                            if (apex.get(i).getX() > apex2.get(i).getX() && apex.get(i).getX() - shape.getVelocity().getVelocityX() <= apex2.get(i).getX())
+                            if (apex.get(i).getX() > apex2.get(i).getX() && apex.get(i).getX() - rectangle.getVelocity().getVelocityX() <= apex2.get(i).getX())
                                 return true;
                         }
                     }
