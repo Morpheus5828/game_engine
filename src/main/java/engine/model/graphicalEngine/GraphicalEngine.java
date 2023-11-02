2b4d0b219ede6209955dab329f41fa68a52251bd
AHAMADA Warren <warren.ahamada@etu.univ-amu.fr>
2023-11-02 14:03:21 +0100

    JAVADOC GraphicalEngine, MapDrawing, PhysicalEngine

--- src/main/java/engine/model/graphicalEngine/GraphicalEngine.java
+++ src/main/java/engine/model/graphicalEngine/GraphicalEngine.java
@@ -10,6 +10,20 @@ public class GraphicalEngine {
     private Group playGround;
     private Canvas canvas;
 
+    /**
+     * Constructor of GraphicalEngine
+     * The role of the GraphicalEngine is to draw the map and the entities.
+     * It is also used to clear the canvas.
+     * The playGround is a JavaFX object that contains the canvas.
+     * The canvas is the place where the map and the entities are drawn.
+     * The canvas is a JavaFX object.
+     * The canvas has the width and the length of the window.
+     * The GraphicsContext is the object that allows to draw on the canvas.
+     * The Group is a JavaFX object that contains the canvas.
+     *
+     * @param width
+     * @param length
+     */
     public GraphicalEngine(double width, double length) {
         this.playGround = new Group();
         this.canvas = new Canvas(width, length);
@@ -18,22 +32,51 @@ public class GraphicalEngine {
         drawMap(width, length, Color.BLACK);
     }
 
+    /**
+     * Return the GraphicsContext of the canvas
+     * @return GraphicsContext
+     */
     public GraphicsContext getGraphicsContext() {
         return this.graphicsContext;
     }
 
+    /**
+     * Return the Group of the canvas
+     * @return Group
+     */
+
     public Group getPlayGround() {
         return this.playGround;
     }
+
+    /**
+     * Draw the map on the canvas
+     * @param width
+     * @param length
+     * @param color
+     *
+     * @see MapDrawing
+     */
 
     public void drawMap(double width, double length, Color color) {
         MapDrawing mapDrawing = new MapDrawing(width, length, color);
         draw(mapDrawing);
     }
 
+    /**
+     * Draw the entities on the canvas
+     * @param drawing
+     *
+     * @see Drawing
+     */
+
     public void draw(Drawing drawing) {
         drawing.draw(this.graphicsContext);
     }
+
+    /**
+     * Clear the canvas
+     */
 
     public void clear() {
         this.graphicsContext.clearRect(0, 0, this.canvas.getWidth(), this.canvas.getHeight());
--- src/main/java/engine/model/physicalEngine/PhysicalEngine.java
+++ src/main/java/engine/model/physicalEngine/PhysicalEngine.java
@@ -7,15 +7,36 @@ import engine.model.physicalEngine.movement.*;
 public class PhysicalEngine {
     private Map map;
 
+    /**
+     * Constructor of the PhysicalEngine.
+     * The PhysicalEngine is the engine that will manage the physics of the game.
+     * It will manage the entity, the collisions, the movements, the map, etc...
+     * By giving the length and the width of the map, the constructor will create a map.
+     *
+     * @param lengthMap
+     * @param widthMap
+     * @see Map
+     */
     public PhysicalEngine(double lengthMap, double widthMap) {
         this.map = new Map(lengthMap, widthMap);
     }
 
+
+    /**
+     * This method will create a @Rectangle and add it to the map.
+     *
+     * @see Rectangle
+     * @see Map
+     */
     public void addEntity() {
         Rectangle entity = new Rectangle(new Position(0, 0), 50, 50, true, new Velocity(0, 0));
         map.addShape(entity);
     }
 
+    /**
+     * This method will return the map of the Engine.
+     * @return the map.
+     */
     public Map getMap() {
         return this.map;
     }
