de80b206e75ce0dd9423ec6999115c4bd40be732
AHAMADA Warren <warren.ahamada@etu.univ-amu.fr>
2023-11-02 13:23:22 +0100

    JAVADOC Movement and Rectangle

--- src/main/java/engine/model/physicalEngine/movement/Movement.java
+++ src/main/java/engine/model/physicalEngine/movement/Movement.java
@@ -7,17 +7,33 @@ import java.util.List;
 
 public class Movement {
 
-    /**
-         Shape has to create a movement which be call for moving.
-     **/
-
 
     private Direction direction;
+
+    /**
+     *
+     * Create a new movement that will be called by a @Rectangle to move in the @Map
+     * We use the different direction listed in the enum @Direction
+     *
+     * @param direction
+     *       The direction of the movement
+     *
+     * @see Direction
+     * @see Rectangle
+     */
 
     public Movement(Direction direction) {
         this.direction = direction;
     }
 
+    /**
+     * Update the X position of the @Rectangle in the @Map after checking if there is nothing to collide with
+     *
+     *
+     * @param direction
+     * @param shape
+     * @param map
+     */
     public void updatePositonX(Direction direction, Rectangle shape, Map map) {
         if(!isColliding(shape, map, direction)){
             if (direction == Direction.RIGHT) {
@@ -28,6 +44,14 @@ public class Movement {
             }
         }
     }
+
+    /**
+     * Update the Y position of the @Rectangle in the @Map after checking if there is nothing to collide with
+     *
+     * @param direction
+     * @param shape
+     * @param map
+     */
 
     public void updatePositonY(Direction direction, Rectangle shape, Map map) {
         if (!isColliding(shape, map, direction)) {
@@ -40,14 +64,34 @@ public class Movement {
         }
     }
 
+    /**
+     * get the direction of the movement
+     *
+     * @return the direction of the movement
+     */
+
     public Direction getDirection() {
         return direction;
     }
 
+    /**
+     * set the direction of the movement
+     *
+     * @param direction
+     */
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
 
+
+    /**
+     * Check if the @Rectangle is colliding with another @Rectangle in the @Map
+     *
+     * @param shape
+     * @param map
+     * @param direction
+     * @return true if the @Rectangle is colliding with another @Rectangle in the @Map
+     */
     public boolean isColliding(Rectangle shape, Map map, Direction direction) {
         List<Position> apex = shape.getApex();
         for (Rectangle shape2 : map.getShapeList()) {
