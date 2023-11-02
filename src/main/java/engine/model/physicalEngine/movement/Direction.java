10b59142091a8ca7daeb6ceec273fdfd395767c1
AHAMADA Warren <warren.ahamada@etu.univ-amu.fr>
2023-11-02 13:34:35 +0100

    JAVADOC Direction and Velocity

--- src/main/java/engine/model/physicalEngine/movement/Direction.java
+++ src/main/java/engine/model/physicalEngine/movement/Direction.java
@@ -1,5 +1,9 @@
 package engine.model.physicalEngine.movement;
 
+/**
+ * This enum is used to represent the direction of a movement. This is basic movements (up, down, left, right) and none if there is no movement.
+ */
+
 public enum Direction {
     UP, DOWN, LEFT, RIGHT, NONE;
 }
--- src/main/java/engine/model/physicalEngine/movement/Velocity.java
+++ src/main/java/engine/model/physicalEngine/movement/Velocity.java
@@ -4,31 +4,72 @@ public class Velocity {
     private double velocityX;
     private double velocityY;
 
+    /**
+     * Constructor of Velocity.
+     * The velocity is basically the speed of an entity.
+     * The velocity is composed of two components: velocityX and velocityY.
+     * The velocityX is the speed of the entity on the x-axis.
+     * The velocityY is the speed of the entity on the y-axis.
+     *
+     * @param velocityX
+     * @param velocityY
+     */
+
     public Velocity(double velocityX, double velocityY) {
         this.velocityX = velocityX;
         this.velocityY = velocityY;
     }
 
+    /**
+     * Return the velocity on the x-axis.
+     * @return velocityX
+     */
     public double getVelocityX() {
         return velocityX;
     }
+
+    /**
+     * Return the velocity on the y-axis.
+     * @return velocityY
+     */
 
     public double getVelocityY() {
         return velocityY;
     }
 
+    /**
+     * Set the velocity on the x-axis.
+     * @param velocityX
+     */
+
     public void setVelocityX(double velocityX) {
         this.velocityX = velocityX;
     }
+
+    /**
+     * Set the velocity on the y-axis.
+     * @param velocityY
+     */
 
     public void setVelocityY(double velocityY) {
         this.velocityY = velocityY;
     }
 
+    /**
+     * Set the velocity on the x-axis and the y-axis.
+     * @param velocityX
+     * @param velocityY
+     */
     public void setVelocity(float velocityX, float velocityY) {
         setVelocityX(velocityX);
         setVelocityY(velocityY);
     }
+
+    /**
+     * Add a bonus to the velocity on the x-axis and the y-axis.
+     * @param bonusX
+     * @param bonusY
+     */
 
     public void addVelocity(double bonusX, double bonusY) {
         setVelocityX(getVelocityX() + bonusX);
