8a932028bb5a1c1fb9a3c69b792d58b0718198ff
di <maximedigiorgio@hotmail.fr>
2023-11-06 17:30:21 +0100

    push classe Ghost

---
+++ src/main/java/engine/gamePlay/aiEngine/Ghost.java
@@ -0,0 +1,9 @@
+package engine.gamePlay.aiEngine;
+
+public class Ghost {
+    public void blinkyMouv(){};
+    public void pinkyMouv(){};
+    public void inkyMouv(){};
+    public void clideMouv(){};
+
+}
